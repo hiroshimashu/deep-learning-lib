9378ca5c7906736daa87b9efdcc003d5cd7377fe
hiroshimashu <hiroshimashu@gmail.com>
2020-05-15 17:32:11 +0900

    Add: section16

--- level1.py
+++ level1.py
@@ -11,16 +11,27 @@ class Variable:
         self.data = data
         self.grad = None
         self.creator = None
+        self.generation = 0
 
     def set_creater(self, func):
         self.creator = func
+        self.generation = func.generation + 1
 
     def backward(self):
 
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
-        funcs = [self.creator]
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.append(f)
+                funcs.sort(key=lambda x: x.generation)
+        
+        add_func(self.creator)
         while funcs:
             f = funcs.pop()
             gys = [output.grad for output in f.outputs]
@@ -28,9 +39,15 @@ class Variable:
             if not isinstance(gxs, tuple):
                 gxs = (gxs,)
             for x, gx in zip(f.inputs, gxs):
-                x.grad = x
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
                 if x.creator is not None:
-                    funcs.append(x.creator)
+                    add_func(x.creator)
+
+    def cleargrad(self):
+        self.grad = None
 
 
 class Function:
@@ -40,6 +57,8 @@ class Function:
         if not isinstance(ys, tuple):
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
+
+        self.generation = max([x.generation for x in inputs])
         for output in outputs:
             output.set_creater(self)
         self.input = input
@@ -108,11 +127,10 @@ def as_array(x):
     return x
 
 
-xs = [Variable(np.array(2)), Variable(np.array(3))]
-f = Add()
-ys = f(xs)
-y = ys[0]
-print(y.data)
+x = Variable(np.array(2.0))
+y = add(x, x)
+y.backward()
+print(x.grad)
 
 
 class SquareTest(unittest.TestCase):
